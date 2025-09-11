940c4645c41dbfd091d13b7543c2d6077631ec73
Fahad Ahmed <fahad24ahmed@gmail.com>
2025-09-11 16:51:58 -0700

    ansible plabook updates and static ip config for bastion

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,56 @@ resource "proxmox_lxc" "bastion" {
   password     = "Container"
   unprivileged = true
   
+  cores = 1     # Logical cores, e.g. 16 Threads of 8 Physical
+  cpulimit = 50 # Uses 50% of 1 Core
+  memory = 128
+  swap = 128
+  onboot = true
+  start = true
+
+  ssh_public_keys = <<-EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbcblSecHjRJmXa/KbHzjjtfCDrqxSCZF/h2C3H4hkP ansible@control
+  EOT
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local-lvm"
+    size    = "1G" # Size should be greater than 600MB for this template
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr1"
+    ip     = "192.168.1.2/24"
+    gw     = "192.168.1.1"
+  }
+
+  /* DHCP Config on LAN
+  network {
+    name   = "eth0"
+    bridge = "vmbr1"
+    ip     = "dhcp"
+  }
+  */
+
+  /* Static IPv4 Config on LAN
+   * Must Set IP w/ CIDR and Gateway
+  network {
+    name   = "eth0"
+    bridge = "vmbr1"
+    ip     = "192.168.1.3/24"
+    gw     = "192.168.1.1"
+  }
+  */
+}
+
+resource "proxmox_lxc" "non_bastion" {
+  target_node  = "proxmox"
+  hostname     = "NONBASTION"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  password     = "Container"
+  unprivileged = true
+  
   cores = 1
   cpulimit = 50 # Uses 50% of 1 Core
   memory = 128
