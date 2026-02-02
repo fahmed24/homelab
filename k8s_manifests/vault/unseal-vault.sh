d74fb4dec7367c57197f8382c0a2cf81f979e866
Fahad <fahad24ahmed@gmail.com>
2026-02-02 15:56:17 -0800

    add vault unseal script

---
+++ k8s_manifests/vault/unseal-vault.sh
@@ -0,0 +1,22 @@
+# Extract unseal keys
+UNSEAL_KEY_1=$(cat vault-init-keys.json | jq -r '.unseal_keys_b64[0]')
+UNSEAL_KEY_2=$(cat vault-init-keys.json | jq -r '.unseal_keys_b64[1]')
+UNSEAL_KEY_3=$(cat vault-init-keys.json | jq -r '.unseal_keys_b64[2]')
+
+# Unseal (need 3 keys)
+kubectl exec -n my-vault my-vault-0 -- \
+  env VAULT_ADDR='https://vault.fahmed.org:8200' \
+  vault operator unseal $UNSEAL_KEY_1
+
+kubectl exec -n my-vault my-vault-0 -- \
+  env VAULT_ADDR='https://vault.fahmed.org:8200' \
+  vault operator unseal $UNSEAL_KEY_2
+
+kubectl exec -n my-vault my-vault-0 -- \
+  env VAULT_ADDR='https://vault.fahmed.org:8200' \
+  vault operator unseal $UNSEAL_KEY_3
+
+# Verify it's unsealed
+kubectl exec -n my-vault my-vault-0 -- \
+  env VAULT_ADDR='https://vault.fahmed.org:8200' \
+  vault status
